669c6a235aa6d79cf0dcd456928477c77b102b5d
Joshyjosh <joshsarnecki@gmail.com>
2022-05-25 15:12:50 -0700

    Initial commit

---
+++ grains.js
@@ -0,0 +1,13 @@
+
+export const square = (n) => {
+  
+  if (n < 1 || n > 64) {
+    throw new Error('square must be between 1 and 64');
+  }
+
+
+};
+
+export const total = () => {
+  throw new Error('Remove this statement and implement this function');
+};
