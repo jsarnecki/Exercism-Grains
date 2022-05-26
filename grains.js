93c24854e9bfb18dbdadee0d17fe53277449cf88
Joshyjosh <joshsarnecki@gmail.com>
2022-05-26 10:38:48 -0700

    All tests passing

--- grains.js
+++ grains.js
@@ -1,13 +1,18 @@
 
 export const square = (n) => {
-  
+
   if (n < 1 || n > 64) {
     throw new Error('square must be between 1 and 64');
   }
 
+  if (n > 51) {
+    return BigInt(2 ** (n - 1));
+  }
+
+  return 2 ** (n - 1);
 
 };
 
 export const total = () => {
-  throw new Error('Remove this statement and implement this function');
+  return BigInt(2 ** 64) - 1n;
 };
